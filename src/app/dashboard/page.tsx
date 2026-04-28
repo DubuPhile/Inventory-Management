ff74efbd54e06b0f24c169c028f037edb49a54fe
Charles <enagecharles27@gmail.com>
2026-04-29 00:07:45 +0800

    Implement Components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import {
+  CheckCircle,
+  Package,
+  Tag,
+  TrendingDown,
+  TrendingUp,
+} from "lucide-react";
+import CardExpenseSummary from "./CardExpenseSummary";
 import CardPurchaseSummary from "./CardPurchaseSuymmary";
 import CardSalesSummary from "./CardSalesSummary";
 import CartPopularProducts from "./CartPopularProducts";
+import StatCard from "./StatCard";
 
 export default function Dashboard() {
   return (
@@ -13,10 +22,64 @@ export default function Dashboard() {
       <CartPopularProducts />
       <CardSalesSummary />
       <CardPurchaseSummary />
-      <div className="row-span-3  bg-gray-500 " />
-      <div className="md:row-span-1 xl:row-span-2 bg-gray-500 " />
-      <div className="md:row-span-1 xl:row-span-2 bg-gray-500 " />
-      <div className="md:row-span-1 xl:row-span-2 bg-gray-500 " />
+      <CardExpenseSummary />
+      <StatCard
+        title="Customer & Expenses"
+        primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 December 2025"
+        details={[
+          {
+            title: "Customer Growth",
+            amount: "250.00",
+            changePercentage: 120,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Expenses Growth",
+            amount: "20.00",
+            changePercentage: -52,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <StatCard
+        title="Dues & Pending Orders"
+        primaryIcon={<CheckCircle className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 December 2025"
+        details={[
+          {
+            title: "Dues",
+            amount: "134.00",
+            changePercentage: 50,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Pending Orders",
+            amount: "156",
+            changePercentage: 62,
+            IconComponent: TrendingUp,
+          },
+        ]}
+      />
+      <StatCard
+        title="Sales & Discount"
+        primaryIcon={<Tag className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 December 2025"
+        details={[
+          {
+            title: "Sales",
+            amount: "1500.00",
+            changePercentage: 160,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Discount",
+            amount: "45.00",
+            changePercentage: -40,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
     </main>
   );
 }
